1104d845ff314dc52fd2e5b5be8d6f2252be6eba
github username <github email address>
2017-12-15 13:36:21 -0500

    port fix 2

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var WebSocketServer = require('ws').Server
 const express = require('express');
 const uuidv4 = require('uuid/v4');
 const app = express();
-var wss = new WebSocketServer({port: process.env.port})
+const PORT = process.env.PORT || 8080
+var wss = new WebSocketServer({port: PORT})
 
 let allConnections = []
 let lobbies = [ [], [], [] ]
@@ -53,4 +54,4 @@ wss.on('connection', (ws, req) => {
   })
 })
 
-console.log("LISTENING FOR WS CONNECTIONS ON PORT: ", 5000);
+console.log("LISTENING FOR WS CONNECTIONS ON PORT: ", PORT);
