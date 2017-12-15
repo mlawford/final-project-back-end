3c8feed7b4dae9388dcb90d7d1321bb37a8e96c8
github username <github email address>
2017-12-15 14:02:43 -0500

    path removed

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const uuidv4 = require('uuid/v4');
 const app = express();
 const PORT = process.env.PORT || 5000
-const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
