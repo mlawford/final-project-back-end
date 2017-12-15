a05212f71f0df09e55a4f4e93cdc1d59a44e641d
github username <github email address>
2017-12-15 14:06:13 -0500

    added path

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var WebSocketServer = require('ws').Server
 const express = require('express');
 const uuidv4 = require('uuid/v4');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000
+const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
