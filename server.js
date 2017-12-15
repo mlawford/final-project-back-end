a50440786c2d6a7a5d5db916fa9ec50b377f3636
github username <github email address>
2017-12-15 13:53:31 -0500

    socket fix 2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const uuidv4 = require('uuid/v4');
 const app = express();
 const PORT = process.env.PORT || 5000
+const path = require('path');
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
