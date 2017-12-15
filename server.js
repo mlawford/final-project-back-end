cf28c83a6919c6284e4b65a7266d7d3c0e82dd62
github username <github email address>
2017-12-15 13:45:22 -0500

    port fix 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var WebSocketServer = require('ws').Server
 const express = require('express');
 const uuidv4 = require('uuid/v4');
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 var wss = new WebSocketServer({port: PORT})
 
 let allConnections = []
